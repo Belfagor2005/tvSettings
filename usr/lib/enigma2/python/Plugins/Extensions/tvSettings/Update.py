3df44faf5b892e7818d65e68d140d1090916f3f2
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2023-07-11 20:22:23 +0200

    Up fix

--- usr/lib/enigma2/python/Plugins/Extensions/tvSettings/Update.py
+++ usr/lib/enigma2/python/Plugins/Extensions/tvSettings/Update.py
@@ -7,7 +7,7 @@ print("Update.py")
 
 
 def upd_done():
-    from os import popen
+    from os import popen, remove
     cmd01 = "wget http://patbuweb.com/tvSettings/tvsettings.tar -O /tmp/tvsettings.tar ; tar -xvf /tmp/tvsettings.tar -C /"
     cmd02 = "wget --no-check-certificate -U 'Enigma2 - tvsettings Plugin' -c 'http://patbuweb.com/tvSettings/tvsettings.tar' -O '/tmp/tvsettings.tar'; tar -xvf /tmp/tvsettings.tar -C /"
     cmd22 = 'find /usr/bin -name "wget"'
@@ -19,6 +19,7 @@ def upd_done():
         popen(cmd02)
     except:
         popen(cmd01)
+    remove('/tmp/tvsettings.tar')
     return
 
 '''
